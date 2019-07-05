86b7f9696ac82755c54d1d22ab9f6bb017d477aa
shakyasaijal <saijalshakya@gmail.com>
2019-07-05 15:35:05 +0545

    feat: api fetch for company details and careers

--- src/components/body.js
+++ src/components/body.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useContext, useState} from 'react';
 import Header from './pages/header';
 import About from './pages/about-us';
 import Service from './pages/services';
@@ -6,9 +6,24 @@ import Reason from './pages/reason';
 import Team from './pages/team';
 import Career from './pages/career';
 import Portfolio from './pages/portfolio';
+import {ModeContext} from '../project_mode';
+import axios from 'axios';
 
 
 function Body() {
+
+  const [data, setData] = useState([]);
+  const mode = useContext(ModeContext)
+
+  useEffect(() => {
+    const fetchData = async () => {
+     await axios.get(mode+"/api/v1/company-details")
+      .then(res => setData(res.data.data))
+      .catch((err) => console.log('Error in teams'+err))
+    };
+  fetchData();
+  }, []);
+
   return (
     <div className="Body">
         <Header/>
@@ -18,6 +33,17 @@ function Body() {
         <Team/>
         <Career/>
         <Portfolio/>
+        <div className="Team">
+          <h1>Team</h1>
+            <ul>
+            {data.map(company => (
+              <li key={company.id}>
+                <a href={company.id}>{company.company_name} - {company.phone}</a>
+                <p>{company.location} - {company.email}</p>
+              </li>
+              ))}
+            </ul>
+        </div>
     </div>
   );
 }
--- src/components/pages/career.js
+++ src/components/pages/career.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useEffect, useContext, useState} from 'react';
+import {ModeContext} from '../../project_mode';
+import axios from 'axios';
+
 
 function Career() {
+
+  const [data, setData] = useState([]);
+  const mode = useContext(ModeContext)
+
+  useEffect(() => {
+    const fetchData = async () => {
+     await axios.get(mode+"/api/v1/careers")
+      .then(res => setData(res.data.data))
+      .catch((err) => console.log('Error in teams'+err))
+    };
+  fetchData();
+  }, []);
+
   return (
     <div className="Career">
-        <h1>Career</h1>
+          <h1>Careers</h1>
+            <ul>
+            {data.map(career => (
+              <li key={career.id}>
+                <a href={career.id}>{career.title}</a>
+                <p>Short: {career.short_description}<br/><br/>Job{career.job_description}</p>
+                <p>Skills: {career.skills}</p>
+              </li>
+              ))}
+            </ul>
     </div>
   );
 }
