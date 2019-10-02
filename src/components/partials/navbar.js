ec2036e007e0179cd6a7092965bc4f7698a0540a
shakyasaijal <saijalshakya@gmail.com>
2019-10-02 15:23:30 +0545

    feat: responsiveness #1

--- src/components/partials/navbar.js
+++ src/components/partials/navbar.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect, useContext, useState} from 'react';
 import logo from '../../static/Images/logo.png';
 
 function Navbar() {
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+          vi()
+    }, []);
   return (
     <div className="Navbar">
       <nav className="navbar navibar">
           <div className="container-fluid contain contain-nav">
               <div className="navbar-header">
-                  <a className="navbar-brand" href="/"><img src={logo} className="logo" /></a>
+                    <a className="navbar-brand" href="/"><img src={logo} className="logo" /></a>
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#navigation" aria-expanded="false">
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
               </div>
 
               <ul className="nav navbar-nav navbar-right">
@@ -29,8 +41,28 @@ function Navbar() {
               </ul>
           </div>
       </nav>
+      
     </div>
   );
 }
 
+let vi = async =>
+{
+    const navbarToggle = document.getElementsByClassName('navbar-toggle')[0];
+const navbarNav = document.getElementsByClassName('navbar-nav')[0];
+const mainContainer = document.getElementsByClassName('main-container')[0];
+
+let triggered = false;
+navbarToggle.onclick = () => {
+    if (!triggered) {
+    navbarNav.style.right = '0';
+    mainContainer.style.right = '14rem';
+} else {
+    navbarNav.style.right= '-14rem';
+    mainContainer.style.right = '0';
+}
+    triggered = !triggered;
+};
+}
+      
 export default Navbar;
